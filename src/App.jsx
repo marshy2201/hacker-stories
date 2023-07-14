f40c7703fe440a7c99f67b8f4e2a38196b7ce4e9
Lewis Marshall <marshy2201@yahoo.co.uk>
2023-07-14 10:51:47 +0100

    form added

--- src/App.jsx
+++ src/App.jsx
@@ -96,28 +96,21 @@ const App = () => {
   }
 
   // handle search submit
-  const handleSearchSubmit = () => setUrl(`${API_ENDPOINT}${searchTerm}`);
+  const handleSearchSubmit = (event) => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`)
+  
+    event.preventDefault();
+  }
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel 
-        id="search"
-        value={searchTerm} 
-        onInputChange={handleSearchInput}
-        isFocused={true}
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
-
-      <button
-        type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Submit
-      </button>
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchSubmit={handleSearchSubmit}
+        onSearchInput={handleSearchInput}
+      />
       <hr />
 
       {stories.isError && <p>Something went wrong...</p>}
@@ -174,5 +167,24 @@ const Item = ({ item, onRemoveItem }) => (
     <button onClick={() => onRemoveItem(item)}>X</button>
   </li>
 );
+
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit
+}) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel 
+      id="search"
+      value={searchTerm} 
+      onInputChange={onSearchInput}
+      isFocused={true}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button type="button" disabled={!searchTerm}>Submit</button>
+  </form>
+)
 
 export default App;
